22de392f0e48e48669113ab9774663395abfced6
Alexandre Klabunde <alexandre@ideatos.com.br>
2023-10-23 22:57:32 -0300

    terminado

--- components/main/Main.vue
+++ components/main/Main.vue
@@ -1,7 +1,6 @@
 <script setup>
-
 // const variants = ["elevated", "flat", "tonal", "outlined"];
- const color = ref("indigo");
+const color = ref("indigo");
 </script> 
  
  <template>
@@ -9,18 +8,25 @@
     <v-row align="center" justify="center">
       <v-col cols="auto">
         <v-radio-group v-model="color" inline>
-          <v-radio label="indigo" color="indigo" value="indigo"></v-radio>
-          <v-radio
-            label="indigo-darken-3"
-            color="indigo-darken-3"
-            value="indigo-darken-3"
-          ></v-radio>
-          <v-radio label="primary" color="primary" value="primary"></v-radio>
-          <v-radio
-            label="secondary"
-            color="secondary"
-            value="secondary"
-          ></v-radio>
+          <v-row align="center" justify="center">
+            <v-col cols="6" max-width="200">
+              <v-text-field
+                persistent-hint
+                v-model.number="newOrder[0]"
+                label="New Order"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6" max-width="200">
+              <v-text-field
+                persistent-hint
+                v-model.number="newOrder[1]"
+                label="New Order"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6" align="center" justify="center" >
+              <v-btn  @click="reorderItems(newOrder[0],newOrder[1])" color="primary">Save</v-btn>
+            </v-col>
+          </v-row>
         </v-radio-group>
       </v-col>
     </v-row>
@@ -34,17 +40,16 @@
         >
           <v-card-item>
             <div>
-              <div class="text-overline mb-1">
-              </div>
-              <div class="text-h6 mb-1">{{items.order}} </div>
+              <div class="text-overline mb-1"></div>
+              <div class="text-h6 mb-1">{{ items.order }}</div>
               <span class="class-period mb-3">
-                {{items.color}}
-          </span>
+                {{ items.id }}
+              </span>
             </div>
           </v-card-item>
 
           <v-card-actions>
-            <v-btn> {{items.color}} </v-btn>
+            <v-btn> {{ items.id }} </v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
@@ -53,20 +58,43 @@
 </template>
 
 <script>
-
 export default {
   name: "Main",
-
   data() {
     return {
       items: [
-        { order: 1, color: `white` },
-        { order: 2, color: 'black' },
-        { order: 3, color: 'white' },
-        { order: 4, color: 'black' },
-        { order: 5, color: 'white' },
+        { order: 1, id: 10 },
+        { order: 2, id: 20 },
+        { order: 3, id: 30 },
+        { order: 4, id: 40 },
+        { order: 5, id: 50 },
+        { order: 6, id: 60 },
+        { order: 7, id: 70 },
+        { order: 8, id: 80 },
+        { order: 9, id: 90 },
+        { order: 10, id: 100 },
       ],
+      newOrder:[]
+
     };
+  },
+  methods: {
+    
+    reorderItems() {
+      const newIndex = parseInt(this.newOrder[0]) - 1;
+      console.log(newIndex)
+      const itemToMove = this.items.splice(this.newOrder[1]-1, 1)[0];
+      console.log(itemToMove)
+
+      this.items.splice(newIndex, 0, itemToMove);
+       this.items.forEach((item, index) => {
+        item.order = index + 1;
+       });
+      this.items.sort((a, b) => a.order - b.order);
+      this.items.forEach((item, index) => {
+        item.order = index + 1;
+      });
+    }
   },
 };
 </script>
